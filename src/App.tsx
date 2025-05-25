4154c5f709c37818b32c67aef952044a39eb99c3
John Enderby <john.enderby.1987@gmail.com>
2025-05-25 22:05:05 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import { Nav } from "./components/Nav";
 import Design from "./pages/Design";
 import Engineering from "./pages/Engineering";
-
-function Home() {
-  return <h1>Home</h1>;
-}
-
-function About() {
-  return <h1>About</h1>;
-}
-
-function NotFound() {
-  return <h1>404: Page not found</h1>;
-}
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Index from "./pages/Index";
 
 export default function App() {
   return (
     <>
       <Nav />
-
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Index />} />
         <Route path="design" element={<Design />} />
         <Route path="engineering" element={<Engineering />} />
         <Route path="about" element={<About />} />
-        {/* wildcard route for unmatched URLs */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
