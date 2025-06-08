45f7038bb258aed325e01f900ac2b7059913291c
John Enderby <john.enderby.1987@gmail.com>
2025-06-08 22:47:21 +0100

    about framework

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,29 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>About me</h1>
+      <img src="../../../../Pictures/1300155.jpg" />
+      <p>Software Engineer & Product Designer</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat esse
+        tempora, numquam, similique adipisci ipsa velit quo doloribus magnam
+        commodi quia recusandae cumque ad aliquid inventore aspernatur itaque
+        magni debitis?
+      </p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <div>Golf</div>
+      <div>BBQ</div>
+      <div>Weightlifting</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat esse
+        tempora, numquam, similique adipisci ipsa velit quo doloribus magnam
+        commodi quia recusandae cumque ad aliquid inventore aspernatur itaque
+        magni debitis?
+      </p>
+      <div>
+        <div>Experience</div>
+        <div>Skills</div>
+      </div>
+    </div>
+  );
 }
