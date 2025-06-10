81b392daaab92c29af9b29807a4aa88a28394fbc
John Enderby <john.enderby.1987@gmail.com>
2025-06-10 23:24:41 +0100

    fonts and themes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,6 +3,7 @@ export default function About() {
     <div>
       <h1>About me</h1>
       <img src="../../../../Pictures/1300155.jpg" />
+      <h2>Heading 2</h2>
       <p>Software Engineer & Product Designer</p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat esse
@@ -14,6 +15,15 @@ export default function About() {
       <div>Golf</div>
       <div>BBQ</div>
       <div>Weightlifting</div>
+      <h3>Heading 3</h3>
+      <p>Software Engineer & Product Designer</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat esse
+        tempora, numquam, similique adipisci ipsa velit quo doloribus magnam
+        commodi quia recusandae cumque ad aliquid inventore aspernatur itaque
+        magni debitis?
+      </p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat esse
         tempora, numquam, similique adipisci ipsa velit quo doloribus magnam
